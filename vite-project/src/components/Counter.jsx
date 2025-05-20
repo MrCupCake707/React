79a597beee98197b6c357bcc7712253ee1df5f08
Ivan.B <vanya.botalov@mail.ru>
2025-05-20 23:06:19 +0500

    typing

--- vite-project/src/components/Counter.jsx
+++ vite-project/src/components/Counter.jsx
@@ -20,7 +20,7 @@ export default function Counter(){
 import { useState } from 'react'
 export default function Counter(){
 
-    const  [count, setCount] = useState(0)
+    const  [count,  setCount] = useState<number>(0)
         function plus () {
             setCount ((prevCount)=> prevCount +1)
         }
