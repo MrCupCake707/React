08126c9afb477428366f2adaae03963283ff5a3c
Ivan.B <vanya.botalov@mail.ru>
2025-05-21 17:45:46 +0500

    ts

--- Vite_ts_test/react/src/App.tsx
+++ Vite_ts_test/react/src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { createElement as e } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return e(type: 'div', props{className: 'container', children:[
+    e(type: 'h1', props:{className: 'front-bold'}, children: 'Test JSX'),
+    e(type: 'button', props:{className: ''}, children: 'Click me!')
+  ])
 }
-
 export default App
